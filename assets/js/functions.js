d564f668315b60fc4bd21057f7bdd2dd0b8af360
Szymon Hernik <szymonhernik123@gmail.com>
2022-03-28 11:03:18 +0200

    added mapStages to the top plus first mapped scrolling

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,4 +2,28 @@ $(document).ready(function() {
 
 // functions go here
 
+// element.scroll({
+//   top: 100,
+//   left: 100,
+//   behavior: 'smooth'
+// });
+
+function mouseMove(event){
+
+  mapStages.scroll({
+    left: event.pageX,
+    behavior: 'smooth'
+  })
+
+    // console.log("pageX: ",event.pageX,
+    // "pageY: ", event.pageY,
+    // "clientX: ", event.clientX,
+    // "clientY:", event.clientY)
+}
+
+let mapStages = document.getElementById('fragments');
+
+
+window.addEventListener('mousemove', mouseMove);
+
 });
