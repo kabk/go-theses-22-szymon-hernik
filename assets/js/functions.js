215849f16a712785a3ee76b7f778f95bc8bdf4ba
Szymon Hernik <szymonhernik123@gmail.com>
2022-03-28 12:33:48 +0200

    scroll top depending on the height of the text container

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -21,9 +21,45 @@ function mouseMove(event){
     // "clientY:", event.clientY)
 }
 
+function scrollProgressBar () {
+
+
+    // let stagesScrollDistance = -(mapStagesWidth)
+
+
+
+    let scrollDistance = -(mainText.getBoundingClientRect().top);
+    let progressPercentage =
+        (scrollDistance /
+            (mainText.getBoundingClientRect().height -
+                document.documentElement.clientHeight)) * 100;
+
+    let val = Math.floor(progressPercentage);
+
+    //here mapStages needs to get value that comes out of
+    //left is ammount:
+
+    let progressStages =
+        -(mapStagesWidth) * progressPercentage /100;
+    mapStages.style.left = progressStages + 'px';
+
+    if (val < 0) {
+        mapStages.style.left = '0';
+    }
+};
+
 let mapStages = document.getElementById('fragments');
+let mainText = document.getElementById('mainText');
+
+let mapStagesWidth = mapStages.offsetWidth;
+
+window.addEventListener('scroll', scrollProgressBar);
+
+// window.addEventListener('mousemove', mouseMove);
+//
 
 
-window.addEventListener('mousemove', mouseMove);
 
+// END OF CODE
+// END OF CODE
 });
